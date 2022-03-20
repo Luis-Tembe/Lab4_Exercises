c19587bc2c3c42d04c2d8ff7df1f4e00893e8916
Luis Tembe <ltembe1@stu.vistula.edu.pl>
2022-03-20 14:35:36 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        RectangleArea obj=new RectangleArea();
+        obj.getData();
+        obj.Computing();
+        obj.DisplayResult();
+
+    }
+    }
+
